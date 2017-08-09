df9909593e8a7e544864e8edfd06126eb5cd28ff
xxxxxKoreanxxxxx <xxxxxKoreanxxxxx@users.noreply.github.com>
2017-08-09 03:38:21 -0400

    Update ChallengerTristana.lua

--- ChallengerTristana.lua
+++ ChallengerTristana.lua
@@ -134,7 +134,7 @@ function ChallengerTristana:Draw()
 	local target = _G.SDK.TargetSelector:GetTarget(900, _G.SDK.DAMAGE_TYPE_PHYSICAL)
 	if self.Menu.Combo.BufferW:Value() and self:CheckSpell(900) and grabTime ~= nil and self:IsReady(_W) then 
 		if myHero.pos:DistanceTo(target.pos) > 350 then
-			if ctc - grabTime >= 28 then
+			if ctc - grabTime >= Game.Latency() - 21 then
 				local jump = myHero.pos:Shortened(target.pos, 700)
 				_G.SDK.Orbwalker:SetMovement(false)
 				_G.SDK.Orbwalker:SetAttack(false)
@@ -143,7 +143,7 @@ function ChallengerTristana:Draw()
 				Control.KeyUp(HK_W)
 			end
 		else
-			if ctc - grabTime >= 12 then
+			if ctc - grabTime >= Game.Latency() - 37 then
 				local jump = myHero.pos:Shortened(target.pos, 700)
 				_G.SDK.Orbwalker:SetMovement(false)
 				_G.SDK.Orbwalker:SetAttack(false)
